ed4b22435ab017ffa669cd69359a2b313747eede
koohii-afk <73545970+koohii-afk@users.noreply.github.com>
2024-09-17 22:37:59 -0400

    Local axes vs World Space

--- Assets/Scripts/AxisDraw.cs
+++ Assets/Scripts/AxisDraw.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class AxesDraw : MonoBehaviour
 {
+    public float length = 1.0f;
+    public bool local = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,13 @@ public class AxesDraw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 start= transform.position;
+        Vector3 right = local ? transform.right: Vector3.right;
+        Vector3 up = local? transform.up: Vector3.up;
+        Vector3 forward = local? transform.forward: Vector3.forward;  
+
+        Debug.DrawLine(start, start + right * length, Color.red);
+        Debug.DrawLine(start, start + up * length , Color.green);
+        Debug.DrawLine(start, start + forward * length , Color.blue);
     }
 }
