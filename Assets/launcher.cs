b5b92e686d2f62bf93e4f3b82d3654d9fad8467e
koohii-afk <73545970+koohii-afk@users.noreply.github.com>
2024-09-28 23:16:43 -0400

    lab3

--- Assets/launcher.cs
+++ Assets/launcher.cs
@@ -4,32 +4,48 @@ public class launcher : MonoBehaviour
 {
     public float angleDegrees=30;
     public float speed=100;
-    public float deltaTime = 0.02f;
+    float dt;
     public Vector3 velocity;
-    public Vector3 gravityAcceleration = new Vector3(0, -10, 0);
+    public Vector3 gravityAcceleration;
+    public float startHeight = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
+
+    private void Start()
+    {  gravityAcceleration = Physics.gravity;
+    dt = 0;
+        velocity.x = 0;
+        velocity.y = 0;
+        transform.position = new Vector3(0, startHeight, 0);
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            velocity = new Vector3(10, 16);
-            transform.position = Vector3.zero;
+            launch();
         }
+        velocity = velocity + gravityAcceleration * dt;
+        transform.position = transform.position + velocity * dt;
     }
+
 
     private void FixedUpdate()
     {
-        Vector3 prevPos = transform.position;
-        transform.position = transform.position + velocity * deltaTime;
-        velocity = velocity + gravityAcceleration * deltaTime;
+       
+        
 
     }
 
     public void launch()
     {
+        dt = 0;
+        transform.position = new Vector3(0, startHeight, 0);
+        dt = Time.fixedDeltaTime;
+        velocity = new Vector3(Mathf.Cos(angleDegrees) * speed, Mathf.Sin(angleDegrees) * speed);
+        velocity.x = Mathf.Rad2Deg;
+        velocity.y = Mathf.Rad2Deg;
 
     }
 }
